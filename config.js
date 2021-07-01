fbac44e4fa7052fdc70c9dd5e41067f6403d7107
Duy-Angel <86791291+Duy-Angel@users.noreply.github.com>
2021-07-01 22:44:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Helu bae!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao anh cứ đi nhầm đường
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Tớ hỏi câu này 😙',
+    desc: 'Cậu phải trả lời thật lòng nhaa ',
+    btnYes: 'Yêu Muỗi nhiều <33',
+    btnNo: 'Không, yêu Muỗi làm gì :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bae lại yêu em <3',
+    btnReply: 'Gửi cho Muỗi <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
-    messDesc: 'yêu cậu',
+    messDesc: 'yêu chó Đức',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
